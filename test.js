281204319b5dbe967760da9c83881ba146fe6b01
az4aaz_ <lucienherve@hotmail.com>
2023-03-01 11:58:51 +0100

    Préfill des test

--- test.js
+++ test.js
@@ -1,5 +1,47 @@
 Country.fill_db()
 
+// Pays dont au moins un pays est frontalier n'est pas dans le même continent
 function outstideTheContinent() {
     
+}
+
+// Pays (possibilité de plusieurs) ayant le plus grand nombre de voisins. Affichez aussi les voisins.
+function moreNeighbors() {
+
+}
+
+//Pays n'ayant aucun voisin
+function neighborless() {
+
+}
+
+// Pays (possibilité de plusieurs) parlant le plus de langues. Affichez aussi les langues.
+function moreLanguage() {
+
+}
+
+//Pays ayant au moins un voisin parlant l'une de ses langues. Affichez aussi les pays voisins et les langues en question.
+function withCommonLanguage() {
+
+}
+
+//Pays sans aucun voisin ayant au moins une de ses monnaies.
+function withoutCommonCurrency() {
+
+}
+
+//Pays triés par ordre décroissant de densité de population.
+function sortingDecreasingDensity() {
+
+}
+
+//Pays ayant plusieurs Top Level Domains Internet
+function moreTopLevelDomains() {
+
+}
+
+//listez tous les pays que l'on peut visiter en passant de l'un à l'autre. Evidemment, seuls les pays frontaliers sont accessibles depuis un pays donné.
+// Paramètre d'entrée : nom_pays
+function veryLongTrip() {
+
 }
