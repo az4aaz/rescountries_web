d0a780c4d63ff935ee4c60d5ce02361d3f39136f
Temnate <nathan.barre035@gmail.com>
2023-03-01 11:00:12 +0100

    avec méthode de classe

--- Currency.class.js
+++ Currency.class.js
@@ -3,10 +3,17 @@ class Currency{
     code;
     symbole;
 
+    static all_currencies=new Array();
+
     constructor(nom,code,symbole){
         this.nom=nom;
         this.code=code;
         this.symbole=symbole;
+        if(Currency.all_currencies
+            .some(curr => curr.code===code))
+        {
+            Currency.push(this);
+        }
     }
 
     toString() {
