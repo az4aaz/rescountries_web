346daec0ee4f776dfc723e6b84aaacd3102e24a1
az4aaz_ <lucienherve@hotmail.com>
2023-03-01 11:01:03 +0100

    ajout du constructeur de la classe country

---
+++ Country.js
@@ -0,0 +1,53 @@
+import countries from './countries.json' assert {type: 'json'};
+// TODO Importer plus proprement parceque dans le sujet ils disent d'importer dans la fonction fill_db
+
+class Country {
+    constructor(
+        alpha3Code,
+        area,
+        borders = null,
+        capital,
+        continent,
+        flags,
+        names,
+        population,
+        topLevelDomain
+    ) {
+        this.alpha3Code = alpha3Code;
+        this.area = area;
+        this.borders = borders;
+        this.capital = capital;
+        this.continent = continent;
+        this.flags = flags;
+        this.names = names;
+        this.population = population;
+        this.topLevelDomain = topLevelDomain;
+    }
+
+    get toString() {
+        return (
+            this.alpha3Code +
+            " - " +
+            this.area +
+            " - " +
+            this.borders +
+            " - " +
+            this.capital +
+            " - " +
+            this.continent +
+            " - " +
+            this.flags +
+            " - " +
+            this.names +
+            " - " +
+            this.population +
+            " - " +
+            this.topLevelDomain +
+            " - "
+        );
+    }
+
+    fill_db() {
+        
+    }
+}
